ead0b8c643157461b218ee25477669ba9ef77b85
MarcEsquillo27 <esquillomarc76@gmail.com>
2024-06-08 10:28:42 +0800

    ssl

--- api/app.js
+++ api/app.js
@@ -1,35 +1,44 @@
-const express = require('express')
-require('dotenv').config()
-const port = 12799
-const helmet = require('helmet')
-const inventory = require('../routes/inventory.js')
-const sales = require('../routes/sales.js')
-const audit = require('../routes/audit.js')
-const category = require('../routes/category.js')
-const discount = require('../routes/discount.js')
-const account = require('../routes/account.js')
-const void_data = require('../routes/void.js')
-const login = require('../routes/login.js')
-const cors = require('cors')
+const express = require('express');
+require('dotenv').config();
+const https = require('https'); // Use the 'https' module instead of 'http'
+const fs = require('fs'); // Node's file system module
+const helmet = require('helmet');
+const inventory = require('../routes/inventory.js');
+const sales = require('../routes/sales.js');
+const audit = require('../routes/audit.js');
+const category = require('../routes/category.js');
+const discount = require('../routes/discount.js');
+const account = require('../routes/account.js');
+const void_data = require('../routes/void.js');
+const login = require('../routes/login.js');
+const cors = require('cors');
+const verifyToken = require('../routes/auth_mid.js');
 
+const app = express();
+app.use(helmet());
+app.use(cors());
 
-// middleware
-const verifyToken = require('../routes/auth_mid.js')
- 
-const app = express()
-app.use(helmet())
+// Specify the path to your SSL certificate and private key
+const sslOptions = {
+	key: fs.readFileSync('/etc/ssl/certs/skriptr.tech.key'),
+	cert: fs.readFileSync('/etc/ssl/certs/skriptr.tech.crt')
+  };
+  
 
-app.use(cors())
- 
-app.use('/inventory' ,verifyToken, inventory)
-app.use('/sales', verifyToken,sales)
-app.use('/audit', verifyToken,audit)
-app.use('/category',verifyToken,category)
-app.use('/discount', verifyToken,discount)
-app.use('/account' , verifyToken,account)
-app.use('/login' ,login)
-app.use('/void',verifyToken,void_data)
- 
-app.listen(port,function(){
-	console.log('listening to port ' + port);
-})
+// Use HTTPS instead of HTTP
+const port = 12799;
+const server = https.createServer(sslOptions, app);
+
+// Use your middleware and routes
+app.use('/inventory', verifyToken, inventory);
+app.use('/sales', verifyToken, sales);
+app.use('/audit', verifyToken, audit);
+app.use('/category', verifyToken, category);
+app.use('/discount', verifyToken, discount);
+app.use('/account', verifyToken, account);
+app.use('/login', login);
+app.use('/void', verifyToken, void_data);
+
+server.listen(port, function () {
+  console.log('HTTPS server listening on port ' + port);
+});
