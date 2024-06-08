c746353ce1cc41a5bfb2d1c2501137d254b803e4
MarcEsquillo27 <esquillomarc76@gmail.com>
2024-06-08 11:36:50 +0800

    changes

--- routes/auth_mid.js
+++ routes/auth_mid.js
@@ -7,7 +7,7 @@ const verifyToken= (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1]
 if (!token) return res.status(401).json({ error: 'Access denied' });
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.secret_key);
         console.log('line11', decoded)
         req.userdetails = decoded.userdetails;
         
